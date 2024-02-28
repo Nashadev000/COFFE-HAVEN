7ea711dbe1b18e4d2f767a20434596bd2f6df4c6
NIEVES <nasha03oh@gmail.com>
2024-02-28 13:56:02 -0500

    style: :art: añadiendo el bot y sus estilos

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Función para mostrar u ocultar el chat
+function toggleChat() {
+    var chatContainer = document.getElementById("chat-container");
+    if (chatContainer.style.display === "none") {
+        chatContainer.style.display = "block";
+    } else {
+        chatContainer.style.display = "none";
+    }
+}
